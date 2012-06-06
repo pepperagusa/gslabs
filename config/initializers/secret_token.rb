f1f62d2ede13eeb2b4e5af9b3d40fa2df1db5432
Peppe <pepperagusa@gmail.com>
2012-06-05 23:36:00 -0700

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gslabs::Application.config.secret_token = '86b56fbaf2eeba0e2d261cf68db8a4180ecaf48bec00e5baadd8f0362d65aa17d79fdc76607919a1e4f6b1bf5923989a393d596f2bd00a07259988c04030577e'
