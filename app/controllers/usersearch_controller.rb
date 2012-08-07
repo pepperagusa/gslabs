1011315a1d2b2c2b38065e6164f195f25a2dfc9c
Peppe <pepperagusa@gmail.com>
2012-08-07 10:28:03 -0700

    with new git

--- app/controllers/usersearch_controller.rb
+++ app/controllers/usersearch_controller.rb
@@ -36,8 +36,8 @@ class UsersearchController < ApplicationController
     session[:member_email] = params['member_email']
 
     @this_member = api_call("/people.json?email=#{session[:member_email]}", session[:username], session[:password])
-    
-    all_topics = api_call("/people/#{@this_member['id']}/topics.json", session[:username], session[:password])["data"]
+
+    all_topics = api_call("/people/#{@this_member['id']}/topics.json?sort=recently_active", session[:username], session[:password])["data"]
     puts "-------username: #{session[:username]} ------------"
 
     @topics = Array.new
